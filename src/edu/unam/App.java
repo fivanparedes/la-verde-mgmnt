3c854360c5c14903679592e5ccd9812c9426dfcc
FacuRodriJ <facurodri00@gmail.com>
2021-07-23 17:47:23 -0300

    Prueba en vscode.

--- src/edu/unam/App.java
+++ src/edu/unam/App.java
@@ -3,5 +3,7 @@ import modelo.*;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        //Haciendo pruebas
     }
 }
