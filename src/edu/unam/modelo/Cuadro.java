5b5d3dda83220835a28796a1324d1e79570ea90e
Rodrigo Lionel Antoniak <rodrigolionelantoniak@gmail.com>
2021-07-25 00:36:00 -0300

    Actualización de tipos de datos y relaciones

--- src/edu/unam/modelo/Cuadro.java
+++ src/edu/unam/modelo/Cuadro.java
@@ -4,6 +4,7 @@ import java.util.*;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 
 public class Cuadro {
@@ -11,57 +12,35 @@ public class Cuadro {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int idCuadro;
 
-    /* Estructura del array puntos:
-    [0] = (a, b)
-    [1] = (c, d)
-    ... = ...
-    [N] = (yn, zn)
-    
-    public int[][] getPuntos(): devuelve la tabla completa.
-    Para obtener un par ordenado en particular, se puede recorrer linealmente ese array o, mas bien, utilizar:
-    public int[] getUnPunto(int index, int[][] p): devuelve un punto (par ordenado -array de dos elementos-).
-    index = indice de 0 a N de donde extraer el par ordenado.
-    p = array en cuestion.
-    Una vez obtenido el punto, es tan simple como operar con los valores de punto[0] y punto[1] como latitud y longitud. */
-    private int[][] puntos;
+    private String descripcion;
+
+    @ManyToOne
     private Lote lote;
+
     @OneToMany(mappedBy = "cuadro")
     private List<Cosecha> cosechas = new ArrayList<>();
 
-    public Cuadro(int[][] puntos, Lote lote, List<Cosecha> cosechas) {
-        this.puntos = puntos;
-        this.lote = lote;
-        this.cosechas = cosechas;
-    }
-    public int getIdCuadro() {
-        return idCuadro;
-    }
-    public void setIdCuadro(int idCuadro) {
-        this.idCuadro = idCuadro;
-    }
-    public int[] getUnPunto(int index, int[][] p) {
-        return puntos[index];
-    }
-    public int[][] getPuntos() {
-        return puntos;
-    }
-    public void setPuntos(int[][] puntos) {
-        this.puntos = puntos;
-    }
-    public Lote getLote() {
-        return lote;
-    }
-    public void setLote(Lote lote) {
-        this.lote = lote;
-    }
-    public List<Cosecha> getCosechas() {
-        return cosechas;
-    }
-    public void setCosechas(List<Cosecha> cosechas) {
-        this.cosechas = cosechas;
-    }
     public Cuadro() {
-        
+        this.setDescripcion("");
     }
-    
+
+    public Cuadro(String descripcion) {
+        this.setDescripcion(descripcion);
+    }
+
+    public int getIdCuadro() {
+        return this.idCuadro;
+    }
+
+    public String getDescripcion() {
+        return this.descripcion;
+    }
+
+    public List<Cosecha> getCosechas() {
+        return this.cosechas;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
 }
--- src/edu/unam/modelo/Lote.java
+++ src/edu/unam/modelo/Lote.java
@@ -1,82 +1,66 @@
 package modelo;
 
 import java.util.*;
-
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 
 public class Lote {
-    public Cosecha getCosecha() {
-        return cosecha;
-    }
-    public void setCosecha(Cosecha cosecha) {
-        this.cosecha = cosecha;
-    }
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int idLote;
 
-    private int[] punto1;
-    private int[] punto2;
-    private int[] initpoint = {0, 0};
+    private double[] punto1;
+    private double[] punto2;
+
+    @ManyToOne
     private Productor productor;
-    private Cosecha cosecha;
 
     @OneToMany(mappedBy = "lote")
     private List<Cuadro> cuadros = new ArrayList<>();
-    
+
+    public Lote() {
+        inicio();
+    }
+
+    public Lote(double[] punto1, double[] punto2) {
+        inicio();
+        this.setPunto1(punto1);
+        this.setPunto2(punto2);
+    }
+
+    public int getIdLote() {
+        return this.idLote;
+    }
+
+    public double[] getPunto1() {
+        return this.punto1;
+    }
+
+    public double[] getPunto2() {
+        return this.punto2;
+    }
+
     public Productor getProductor() {
         return productor;
     }
-    public void setProductor(Productor productor) {
-        this.productor = productor;
+
+    public List<Cuadro> getCuadros() {
+        return cuadros;
     }
 
-    public Lote(int[] punto1, int[] punto2) {
+    public void setPunto1(double[] punto1) {
         this.punto1 = punto1;
-        this.punto2 = punto2;
-        this.productor = new Productor();
-    }
-    public Lote() {
-        this.punto1 = initpoint;
-        this.punto2 = initpoint;
-        this.productor = new Productor();
     }
 
-    public Lote(int[] punto1, int[] punto2, Productor productor) {
-        this.punto1 = punto1;
+    public void setPunto2(double[] punto2) {
         this.punto2 = punto2;
-        this.productor = productor;
-    }
-    
-    public int getIdLote() {
-        return idLote;
-    }
-    public void setIdLote(int idLote) {
-        this.idLote = idLote;
-    }
-    public int[] getPunto1() {
-        return punto1;
-    }
-    public void setPunto1(int[] punto1) {
-        this.punto1 = punto1;
-    }
-    public int[] getPunto2() {
-        return punto2;
-    }
-    public void setPunto2(int[] punto2) {
-        this.punto2 = punto2;
-    }
-    
-    public void setPunto1(int p1, int p2) {
-        this.punto1[0] = p1;
-        this.punto1[1] = p2;
     }
 
-    public void setPunto2(int p1, int p2) {
-        this.punto2[0] = p1;
-        this.punto2[1] = p2;
+    private void inicio() {
+        this.punto1 = new double[2];
+        this.punto2 = new double[2];
     }
 }
