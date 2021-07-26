79147a187314f9b15a5e67e364ff6965ef802387
Rodrigo Lionel Antoniak <rodrigolionelantoniak@gmail.com>
2021-07-26 10:49:23 -0300

    Corrección de relación de las subclases en la BD

--- src/edu/unam/modelo/Cosecha.java
+++ src/edu/unam/modelo/Cosecha.java
@@ -11,10 +11,9 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
 
 @Entity
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Cosecha {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
