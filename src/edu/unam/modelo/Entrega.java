51415da7a0a59f01a3ab356446053d53c1564a96
Rodrigo Lionel Antoniak <rodrigolionelantoniak@gmail.com>
2021-07-27 19:26:34 -0300

    Agregado de tiempo de entrega y registro

--- src/edu/unam/modelo/Entrega.java
+++ src/edu/unam/modelo/Entrega.java
@@ -1,28 +1,65 @@
 package edu.unam.modelo;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import jakarta.persistence.Entity;
 
 @Entity
 public class Entrega extends Cosecha {
     private float pesoEnt;
+    private LocalDateTime tiempoEnt;
 
     public Entrega() {
         super();
         this.setPesoEnt(0.0f);
+        this.setTiempoEnt(LocalDateTime.now());
+    }
+
+    public Entrega(float pesoEnt) {
+        super();
+        this.setPesoEnt(pesoEnt);
+        this.setTiempoEnt(LocalDateTime.now());
     }
 
     public Entrega(float pesoEnt, LocalDate fecha) {
         super(fecha);
         this.setPesoEnt(pesoEnt);
+        this.setTiempoEnt(LocalDateTime.now());
+    }
+
+    public Entrega(float pesoEnt, LocalDate fecha, LocalDateTime tiempoEnt) {
+        super(fecha);
+        this.setPesoEnt(pesoEnt);
+        this.setTiempoEnt(tiempoEnt);
+    }
+
+    public Entrega(float pesoEnt, LocalDate fecha, List<Cuadro> cuadros, List<Empleado> empleados, Secadero secadero) {
+        super(fecha, cuadros, empleados, secadero);
+        this.setPesoEnt(pesoEnt);
+        this.setTiempoEnt(LocalDateTime.now());
+    }
+
+    public Entrega(float pesoEnt, LocalDate fecha, List<Cuadro> cuadros, List<Empleado> empleados, Secadero secadero,
+            LocalDateTime tiempoEnt) {
+        super(fecha, cuadros, empleados, secadero);
+        this.setPesoEnt(pesoEnt);
+        this.setTiempoEnt(tiempoEnt);
     }
 
     public float getPesoEnt() {
         return this.pesoEnt;
     }
 
+    public LocalDateTime getTiempoEnt() {
+        return this.tiempoEnt;
+    }
+
     public void setPesoEnt(float pesoEnt) {
         this.pesoEnt = pesoEnt;
+    }
+
+    public void setTiempoEnt(LocalDateTime tiempoEnt) {
+        this.tiempoEnt = tiempoEnt;
     }
 }
--- src/edu/unam/modelo/Registro.java
+++ src/edu/unam/modelo/Registro.java
@@ -1,28 +1,65 @@
 package edu.unam.modelo;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import jakarta.persistence.Entity;
 
 @Entity
 public class Registro extends Cosecha {
     private float pesoReg;
+    private LocalDateTime tiempoReg;
 
     public Registro() {
         super();
         this.setPesoReg(0.0f);
+        this.setTiempoReg(LocalDateTime.now());
+    }
+
+    public Registro(float pesoReg) {
+        super();
+        this.setPesoReg(pesoReg);
+        this.setTiempoReg(LocalDateTime.now());
     }
 
     public Registro(float pesoReg, LocalDate fecha) {
         super(fecha);
         this.setPesoReg(pesoReg);
+        this.setTiempoReg(LocalDateTime.now());
+    }
+
+    public Registro(float pesoReg, LocalDate fecha, LocalDateTime tiempoReg) {
+        super(fecha);
+        this.setPesoReg(pesoReg);
+        this.setTiempoReg(tiempoReg);
+    }
+
+    public Registro(float pesoEnt, LocalDate fecha, List<Cuadro> cuadros, List<Empleado> empleados, Secadero secadero) {
+        super(fecha, cuadros, empleados, secadero);
+        this.setPesoReg(pesoEnt);
+        this.setTiempoReg(LocalDateTime.now());
+    }
+
+    public Registro(float pesoEnt, LocalDate fecha, List<Cuadro> cuadros, List<Empleado> empleados, Secadero secadero,
+            LocalDateTime tiempoReg) {
+        super(fecha, cuadros, empleados, secadero);
+        this.setPesoReg(pesoEnt);
+        this.setTiempoReg(tiempoReg);
     }
 
     public float getPesoReg() {
         return this.pesoReg;
     }
 
+    public LocalDateTime getTiempoEnt() {
+        return this.tiempoEnt;
+    }
+
     public void setPesoReg(float pesoReg) {
         this.pesoReg = pesoReg;
+    }
+
+    public void setTiempoReg(LocalDateTime tiempoReg) {
+        this.tiempoReg = tiempoReg;
     }
 }
