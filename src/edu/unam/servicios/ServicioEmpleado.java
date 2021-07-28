1207da339a5928004d9b6cb2f8bdf07293b8d14b
fivanparedes <84539585+fivanparedes@users.noreply.github.com>
2021-07-28 16:08:47 -0300

    Update ServicioEmpleado.java

--- src/edu/unam/servicios/ServicioEmpleado.java
+++ src/edu/unam/servicios/ServicioEmpleado.java
@@ -1,48 +1,4 @@
 package edu.unam.servicios;
-import edu.unam.repositorio.Repositorio;
-
-import java.time.LocalDate;
-import java.util.List;
-
-import edu.unam.modelo.Empleado;
-
-public class ServicioEmpleado {
-    private Repositorio repo;
-
-    public ServicioEmpleado(Repositorio r) {
-        this.repo = r;
-    }
-
-    public List<Empleado> listarEmpleados() {
-        return this.repo.buscarTodos(Empleado.class);
-    }
-
-    public Empleado buscarEmpleado(Long idEmpleado) {
-        return this.repo.buscar(Empleado.class, idEmpleado);
-    }
-
-    public void agregarEmpleado(String nombres, String apellidos, int dni, String legajo, LocalDate fechaIngreso, LocalDate nacimiento, long cuil) {
-        if (nombres.trim().length() == 0 || apellidos.trim().length() == 0 || legajo.trim().length() == 0) {
-            throw new IllegalArgumentException("Faltan datos");
-        }
-        this.repo.iniciarTransaccion();
-        Empleado empleado = new Empleado(legajo.toUpperCase().trim(), dni, apellidos.toUpperCase().trim(), nombres.toUpperCase().trim(), fechaIngreso, nacimiento, cuil);
-        this.repo.insertar(empleado);
-        this.repo.confirmarTransaccion();
-    }
-
-    // cambiar valor devuelto (por ejemplo: True ok, False problemas)
-    public void editarEmpleado(Long idEmpleado, String nombres, String apellidos) {
-        if (nombres.trim().length() == 0 || apellidos.trim().length() == 0) {
-            throw new IllegalArgumentException("Faltan datos");
-        }
-        this.repo.iniciarTransaccion();
-        Empleado empleado = this.repo.buscar(Empleado.class, idEmpleado);
-        if (empleado != null) {
-            empleado.setApellidos(apellidos);
-            empleado.setNombres(nombres);
-            // implementar comparable o comparator
-            // o si el id es unico pueden compararar por idpackage edu.unam.servicios;
 
 import edu.unam.repositorio.Repositorio;
 
@@ -175,33 +131,6 @@ public class ServicioEmpleado {
             listaCosechas.remove(cosecha);
             empleado.setCosechas(listaCosechas);
             this.repo.modificar(empleado);
-            this.repo.confirmarTransaccion();
-            return 0;
-        } else {
-            this.repo.descartarTransaccion();
-            return 1;
-        }
-    }
-}
-
-            /* if (! empleado.getDepartamento().equals(departamento)) {
-                empleado.getDepartamento().getEmpleados().remove(empleado);
-                empleado.setDepartamento(departamento);
-                departamento.getEmpleados().add(empleado);
-            } */
-            this.repo.modificar(empleado);
-            this.repo.confirmarTransaccion();
-        } else {
-            this.repo.descartarTransaccion();
-        }
-    }
-
-    public int eliminarEmpleado(Long idEmpleado) {
-        this.repo.iniciarTransaccion();
-        Empleado empleado = this.repo.buscar(Empleado.class, idEmpleado);
-        // hacer todos los chequeos necesarios antes de eliminar
-        if (empleado != null) {
-            this.repo.eliminar(empleado);
             this.repo.confirmarTransaccion();
             return 0;
         } else {
